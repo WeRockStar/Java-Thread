fb7ca21de8ae394b71618301492acbfa6558e4f7
WeRockStar <vr_freestyle@outlook.com>
2015-09-05 03:14:27 +0700

    new thread

--- synchedthread/SynchedMain.java
+++ synchedthread/SynchedMain.java
@@ -1,5 +1,15 @@
-
 package synchedthread;
+
 public class SynchedMain {
-    
+
+    public static void main(String[] args) {
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        
+                    }
+                }
+        );
+    }
 }
