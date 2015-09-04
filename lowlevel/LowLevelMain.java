34069c75a415007dd43f445fe1040047608c8cfe
WeRockStar <vr_freestyle@outlook.com>
2015-09-05 04:16:40 +0700

    run thread low level

--- lowlevel/LowLevelMain.java
+++ lowlevel/LowLevelMain.java
@@ -1,4 +1,17 @@
 package lowlevel;
+
 public class LowLevelMain {
-    
+
+    public static void main(String[] arsg) throws InterruptedException {
+        ThreadTask thread = new ThreadTask(4);
+
+        new Thread(thread).start();
+        synchronized (thread) {
+            thread.wait();
+        }
+        int[] currentScore = thread.getScores();
+        for (int score : currentScore) {
+            System.out.println(score + " ");
+        }
+    }
 }
--- lowlevel/ThreadTask.java
+++ lowlevel/ThreadTask.java
@@ -1,4 +1,37 @@
 package lowlevel;
-public class ThreadTask {
-    
+
+public class ThreadTask implements Runnable {
+
+    private static int[] scores;
+    private int[] source = {1, 2, 3, 4, 5};
+
+    public ThreadTask(int num) {
+        if (num > source.length) {
+            num = source.length;
+            System.out.println("Limiting to " + source.length);
+        }
+        //define length of array
+        scores = new int[num];
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < scores.length; i++) {
+                System.out.println(i + 1);
+                //sleep 1s
+                Thread.sleep(1000);
+                scores[i] = source[i];
+            }
+            synchronized (this) {
+                this.notifyAll();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    public synchronized int[] getScores() {
+        return scores;
+    }
 }
