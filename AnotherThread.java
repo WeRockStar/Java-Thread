c2aa8e51be8b4ba76d7368a830c9f74c8898ca4a
WeRockStar <vr_freestyle@outlook.com>
2015-09-05 02:50:43 +0700

    run thread

--- AnotherThread.java
+++ AnotherThread.java
@@ -4,7 +4,9 @@ public class AnotherThread implements Runnable{
 
     @Override
     public void run() {
-        
+        while(true){
+            System.out.println("Another Thread...");
+        }
     }
     
 }
--- MainThread.java
+++ MainThread.java
@@ -1,6 +1,9 @@
 package threadsimple;
 public class MainThread {
     public static void main(String[] args) {
-        
+        (new Thread(new AnotherThread())).start();
+        while (true) {            
+            System.out.println("main thread");
+        }
     }   
 }
