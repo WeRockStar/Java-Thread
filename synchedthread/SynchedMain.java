68385d37d2333a3613ae49b1332429afeac530ad
WeRockStar <vr_freestyle@outlook.com>
2015-09-05 03:20:39 +0700

    run thread

--- synchedthread/SynchedMain.java
+++ synchedthread/SynchedMain.java
@@ -1,15 +1,29 @@
 package synchedthread;
 
 public class SynchedMain {
-
-    public static void main(String[] args) {
+public static volatile Boolean shouldStop;
+    public static void main(String[] args) {        
         new Thread(
                 new Runnable() {
                     @Override
                     public void run() {
-                        
+                        shouldStop = new Boolean(false);
+                        //5 sec
+                        final long wait =  System.currentTimeMillis() + 5000;
+                        for(;;){
+                           synchronized(shouldStop){
+                               shouldStop = System.currentTimeMillis() > wait;
+                               System.out.println("thread : " + shouldStop);
+                           }
+                        }
                     }
                 }
-        );
+        ).start();
+        
+        for(;;){
+            if(shouldStop){
+                System.exit(0);
+            }
+        }
     }
 }
